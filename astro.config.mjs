814daaa031084cf7460b68a3a4027c55499e37b3
Gigi <47110839+DarthGigi@users.noreply.github.com>
2022-11-06 11:40:52 +0100

    Fixed paths, removed alpinejs, added compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,25 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import image from "@astrojs/image";
-import alpinejs from "@astrojs/alpinejs";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
+  site: "https://sirius-one.vercel.app/",
   integrations: [
     image(),
-    alpinejs(),
     prefetch(),
     sitemap(),
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
+    compress(),
   ],
 });
